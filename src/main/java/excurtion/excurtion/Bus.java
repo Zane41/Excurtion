b72a066451664d6278cb14fbdb5ebd1469452df3
Zane41 <korener@rambler.ru>
2024-04-22 12:37:33 +0300

    готово

--- src/main/java/excurtion/excurtion/Bus.java
+++ src/main/java/excurtion/excurtion/Bus.java
@@ -1,30 +1,20 @@
 package excurtion.excurtion;
 
-import static excurtion.excurtion.HelloController.vvodKolva;
-import static excurtion.excurtion.HelloController.vvodVysoty;
+
 
 public class Bus {
 
-    public static int vvodKolva() {
-        return Integer.parseInt(vvodKolva.getText());
-    }
-
-    public static int[] vvodVisot(int kolvo) {
-        int[] visoty = new int[kolvo];
-        for (int i = 0; i < kolvo; i++) {
-            visoty[i] = Integer.parseInt(vvodVysoty.getText());
-        }
-        return visoty;
-    }
 
     public static String check(int[] visoty) {
-        int min = 437; String otvet = null;
+        int min = 437; String otvet = null; int temp =1;
         for (int i = 0; i < visoty.length; i++) {
-            if (visoty[i] > min)
+            if (visoty[i] > min){
+                temp++;
                 if (visoty.length - 1 == i)
-                    otvet = "No Crash";
+                    otvet = "No Crash";}
             else {
-                    otvet = "Crash "+i;
+
+                otvet = STR."Crash \{temp}";
                     break;
                 }
         }
